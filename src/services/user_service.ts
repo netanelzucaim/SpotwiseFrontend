5c879d0cef1b5f6c9155ad497f24c2c22d5aa06a
danacohen111 <danac2121@gmail.com>
2025-03-29 17:09:15 +0300

    fix

--- src/services/user_service.ts
+++ src/services/user_service.ts
@@ -30,7 +30,7 @@ export interface IUser {
 
 const login = async (user: IUser) => {
     const abortController = new AbortController();
-    const credentials = { email: user.email, password: user.password }
+    const credentials = { username: user.username, password: user.password }
     const response = await apiClient.post('/auth/login', credentials, { signal: abortController.signal });
     const { accessToken, refreshToken, _id } = response.data;
     localStorage.setItem('accessToken', accessToken);
