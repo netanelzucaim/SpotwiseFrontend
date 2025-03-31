cabd218f8ad5d654282d6760f51315454815586c
David Sergeev <prosian25@gmail/com>
2025-03-31 21:07:50 +0300

    made a beautiful map with a panel on the left to write description and etc.

--- src/components/Map/MapPage.tsx
+++ src/components/Map/MapPage.tsx
@@ -1,3 +1,4 @@
+import { FC } from "react";
 import React, { useRef, useEffect } from 'react';
 import * as maptilersdk from '@maptiler/sdk';
 import "@maptiler/sdk/dist/maptiler-sdk.css";
@@ -5,10 +6,10 @@ import '../../styles/MapPage.css';
 import { MAPTILER_API_KEY } from '../../config';
 
 
-export default function Map() {
+const MapPage: FC = () => {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const tokyo = { lng: 34.792501, lat: 31.973001 };
+  const rishonLezion = { lng: 34.792501, lat: 31.973001 };
   const zoom = 14;
   maptilersdk.config.apiKey = MAPTILER_API_KEY;
 
@@ -18,7 +19,7 @@ export default function Map() {
     map.current = new maptilersdk.Map({
       container: mapContainer.current,
       style: maptilersdk.MapStyle.STREETS,
-      center: [tokyo.lng, tokyo.lat],
+      center: [rishonLezion.lng, rishonLezion.lat],
       zoom: zoom
     });
 
@@ -26,11 +27,19 @@ export default function Map() {
       .setLngLat([34.792501,31.973001])
       .addTo(map.current);
 
-  }, [tokyo.lng, tokyo.lat, zoom]);
+  }, [rishonLezion.lng, rishonLezion.lat, zoom]);
 
   return (
-    <div className="map-wrap">
-      <div ref={mapContainer} className="map" />
+    <div className="map-page-container">
+      <div className="info-panel">
+        <h2>Info Panel</h2>
+        <p>This area can contain details about the map, location, etc.</p>
+      </div>
+      <div className="map-wrap">
+        <div ref={mapContainer} className="map" />
+      </div>
     </div>
   );
 }
+
+export default MapPage;
