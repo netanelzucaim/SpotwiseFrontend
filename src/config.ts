37f79d320cb8d1708d9dfdf5c8dd098da82379c6
root <root@ST25-144.cs.colman.ac.il>
2025-05-16 13:48:26 +0300

    finish working on it

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,4 @@ export const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 export const MAPTILER_API_KEY = import.meta.env.VITE_MAPTILER_API_KEY;
 export const HUGGING_API_KEY = import.meta.env.VITE_HUGGING_API_KEY;
 export const BASE_URL = import.meta.env.VITE_BASE_URL
+export const FRONTEND_PORT = import.meta.env.VITE_FRONTEND_PORT;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
+import { FRONTEND_PORT } from "../config"; 
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
-  server: {
-    allowedHosts: ['spotwise.cs.colman.ac.il'], // 👈 allows external access
-    host: '0.0.0.0', // 👈 allows external access
-  }
-})
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': process.env,
+    },
+    server: {
+      allowedHosts: isProduction ? ['spotwise.cs.colman.ac.il'] : 'all',
+      host: '0.0.0.0',
+      https: isProduction
+        ? {
+            key: fs.readFileSync(path.resolve(__dirname, 'cert/myserver.key')),
+            cert: fs.readFileSync(path.resolve(__dirname, 'cert/CSB.crt')),
+          }
+        : false,
+      port: FRONTEND_PORT 
+    },
+  };
+});
