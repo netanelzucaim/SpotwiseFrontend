0df008515450d0074cc7de35773d7dfcc14caa2d
root <root@ST25-144.cs.colman.ac.il>
2025-05-16 15:14:40 +0300

    adding dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import path from 'path'
-import { FRONTEND_PORT } from "../config"; 
+import dotenv from 'dotenv'
 
+// Load the appropriate .env file based on the mode
 export default defineConfig(({ mode }) => {
+  const envFile = `.env.${mode}`;
+  dotenv.config({ path: envFile });
+
   const isProduction = mode === 'production';
+  const FRONTEND_PORT = parseInt(process.env.VITE_FRONTEND_PORT , 10);
 
   return {
     plugins: [react()],
@@ -20,8 +25,8 @@ export default defineConfig(({ mode }) => {
             key: fs.readFileSync(path.resolve(__dirname, 'cert/myserver.key')),
             cert: fs.readFileSync(path.resolve(__dirname, 'cert/CSB.crt')),
           }
-        : false,
-      port: FRONTEND_PORT 
+        : false, // Disable HTTPS in non-production mode
+      port: FRONTEND_PORT, // Use the port from the appropriate .env file
     },
   };
 });
