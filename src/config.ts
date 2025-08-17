f904f65c808547f04907414b70943194ed39b3a6
Your Name <you@example.com>
2025-08-17 14:59:29 +0300

    adding proccess env

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 export const MAPTILER_API_KEY = import.meta.env.VITE_MAPTILER_API_KEY;
-export const HUGGING_API_KEY = import.meta.env.VITE_HUGGING_API_KEY;
-export const BASE_URL = import.meta.env.VITE_BASE_URL
+export const HUGGING_API_KEY = import.meta.env.VITE_HUGGING_API_KEY || process.env.HUGGING_API_KEY;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
