b57968c3de47ab6b8288b181b4aa60e27980f63d
Your Name <you@example.com>
2025-08-17 15:07:19 +0300

    changing the env to compatible to server

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 export const MAPTILER_API_KEY = import.meta.env.VITE_MAPTILER_API_KEY;
+// process.env.HUGGING_API_KEY - when running at production retrieve from env
 export const HUGGING_API_KEY = import.meta.env.VITE_HUGGING_API_KEY || process.env.HUGGING_API_KEY;
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
